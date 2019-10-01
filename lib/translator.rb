623e599819ffaa1d0010e6a04ba6c8712c5a997f
Gene H Li <ghl234@nyu.edu>
2019-10-01 20:01:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,40 @@
 # require modules here
+require 'yaml'
 
-def load_library
+def load_library(filePath)
   # code goes here
+  
+  lib = YAML.load_file(filePath)
+  
+  newHash = new.Hash
+  
+  newHash = {
+    get_meaning: new.Hash,
+    get_emoticon: new.Hash
+  }
+  
+  #iterate through each key and place appropriately
+  # lib[key][0] is English emoticon
+  # lib[key][1] is Japanese equivalent
+  lib.each_key { |n| 
+  
+    
+  
+  
+  }
+  
+  
+  
+  
+  lib
+  
 end
 
 def get_japanese_emoticon
   # code goes here
+  
+  
+  
 end
 
 def get_english_meaning
